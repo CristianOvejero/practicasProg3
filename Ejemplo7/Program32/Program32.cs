dece94e6e23280b02cf99d6994d7eb40d2703e1b
CristianOvejero <cristianovej@gmail.com>
2026-04-10 23:19:03 -0300

    subo bien carpeta ejemplo 7

---
+++ Ejemplo7/Program32/Program32.cs
@@ -0,0 +1,25 @@
+﻿namespace Programa32;
+
+class Program32
+{
+    static void Main(string[] args)
+    {
+        const string NOMBRE_APP = "GameMaster Pro";
+        const int EDAD_MINIMA = 18;
+
+        Console.WriteLine("=== " + NOMBRE_APP + " ===");
+        Console.WriteLine("Restricción: Mayores de " + EDAD_MINIMA + " años.");
+
+        Console.Write("Ingrese su edad: ");
+        int edadCliente = int.Parse(Console.ReadLine());
+
+        bool puedeIngresar = edadCliente >= EDAD_MINIMA;
+
+        Console.WriteLine("Tu edad es: " + edadCliente);
+        Console.WriteLine("¿Puede ingresar?: " + puedeIngresar);
+
+        Console.WriteLine("Presiona ENTER para cerrar.");
+        Console.ReadLine();
+
+    }
+}
---
+++ Ejemplo7/Program33/Program33.cs
@@ -0,0 +1,24 @@
+﻿namespace Programa33;
+
+class Program33
+{
+    static void Main(string[] args)
+    {
+        const string NOMBRE_APP = "Montaña rusa";
+        const double ALTURA_MINIMA = 150;
+
+        Console.WriteLine("=== " + NOMBRE_APP + " ===");
+        Console.WriteLine("Restricción: Altura mínima de " + ALTURA_MINIMA + " centimetros.");
+
+        Console.Write("Ingrese su altura en centimetros por favor ");
+        double alturaCliente = double.Parse(Console.ReadLine());
+
+        bool puedeIngresar = alturaCliente >= ALTURA_MINIMA;
+
+        Console.WriteLine("Tu altura es: " + alturaCliente + " m");
+        Console.WriteLine("¿Puede ingresar?: " + puedeIngresar);
+
+        Console.WriteLine("\nPresiona ENTER para cerrar.");
+        Console.ReadLine();
+    }
+}
